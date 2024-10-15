cba9457a98cbacf4d7c83ec3f0b93eddab9d0255
BungeeDragon <skillmanesther@gmail.com>
2024-10-15 15:29:10 +0100

    Node.js stack using Express and Docker

--- demoDocker/frontend-nginx-app/main.js
+++ demoDocker/frontend-nginx-app/main.js
@@ -2,3 +2,12 @@ window.onload = function () {
     alert("Hi!!!");
 }
 
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        // Typical action to be performed when the document is ready:
+        document.getElementById("demo").innerHTML = xhttp.responseText;
+    }
+};
+xhttp.open("GET", "filename", true);
+xhttp.send();
